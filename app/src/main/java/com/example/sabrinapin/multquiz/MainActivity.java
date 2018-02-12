b60249cfea4d55c50827a24a88aafe826ddaa7c8
Sabrina Pin <sabrinapin@Sabrinas-MacBook-Pro.local>
2018-02-11 21:47:07 -0500

    buttons!!!!!!!!!!!

--- app/src/main/java/com/example/sabrinapin/multquiz/MainActivity.java
+++ app/src/main/java/com/example/sabrinapin/multquiz/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.sabrinapin.multquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
