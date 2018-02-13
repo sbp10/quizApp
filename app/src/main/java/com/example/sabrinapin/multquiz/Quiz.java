aa83a881bf5c43f893756224f144eb2399c66e78
nschroder99 <36391944+nschroder99@users.noreply.github.com>
2018-02-13 01:22:03 -0500

    Update Quiz.java

--- app/src/main/java/com/example/sabrinapin/multquiz/Quiz.java
+++ app/src/main/java/com/example/sabrinapin/multquiz/Quiz.java
@@ -7,22 +7,25 @@ import java.util.Arrays;
  */
 
 public class Quiz {
-    private ArrayList<Question> mQuestions;
-    private final String mTitle;
+    private ArrayList<Question> mQuestions; //list of questions
+    private final String mTitle; //title of quiz
 
     public Quiz(String name, Question[] qs)  {
         mTitle = name;
         mQuestions = new ArrayList<Question>(Arrays.asList(qs));
     }
 
+    //how many questions are in the quiz
     public int size()  {
         return myQuestions.size();
     }
 
+    //what is name of quiz
     public String getName(){
         return myQuestions.size();
     }
 
+    //get me a mf question
     public Question getQuestion(int index){
         if (0 <= index && index < myQuestions.size()){
             return myQuestions.get(index);
