74c55217601414bf12fb6442e4c18e9df52972d1
nschroder99 <36391944+nschroder99@users.noreply.github.com>
2018-02-13 11:54:38 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/sabrinapin/multquiz/MainActivity.java
+++ app/src/main/java/com/example/sabrinapin/multquiz/MainActivity.java
@@ -9,9 +9,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private QuizLibrary mQuiz = new QuizLibrary();
+     private QuizLibrary mQuiz = new QuizLibrary();
 
     private TextView mScoreView;
+    private TextView mScoreText;
     private TextView mQuestionView;
 
 
@@ -28,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton1 = (Button) this.findViewById(R.id.button1);
+        mButton2 = (Button) this.findViewById(R.id.button2);
+        mButton3 = (Button) this.findViewById(R.id.button3);
+        mButton4 = (Button) this.findViewById(R.id.button4);
+        mScoreView = (TextView) this.findViewById(R.id.score);
+        mScoreText = (TextView) this.findViewById(R.id.score_text);
+
+
     }
 
     //update question method
