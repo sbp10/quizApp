5e995235307ea84e8c26e6338b914adee1938959
sbp10 <36347006+sbp10@users.noreply.github.com>
2018-02-17 12:33:03 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/sabrinapin/multquiz/MainActivity.java
+++ app/src/main/java/com/example/sabrinapin/multquiz/MainActivity.java
@@ -5,14 +5,15 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.widget.TextView;
-
+import android.content.Context;
+import android.widget.Toast;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-     private QuizLibrary mQuiz = new QuizLibrary();
+    private Quiz mQuiz;
 
     private TextView mScoreView;
-    private TextView mScoreText;
     private TextView mQuestionView;
 
 
@@ -22,25 +23,88 @@ public class MainActivity extends AppCompatActivity {
     private Button mButton4;
 
     private String mAnswer;
+
     private int mScore;
-    private int mQuestionNum;
+    private int mQuestionNum; //index used to determine when quiz is over and increments as questions progress
+    private int mCorrect; //increments when user inputs correct
+    private int scoreBase; //how many questions there are
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mButton1 = (Button) this.findViewById(R.id.button1);
-        mButton2 = (Button) this.findViewById(R.id.button2);
+        mQuestionView = (TextView) this.findViewById(R.id.question_text);
+        mScoreView = (TextView) this.findViewById(R.id.score_view);
+       // scoreBase = mScoreView.getText().toString(); work on this in xml file perhaps
+        mButton1 =  (Button) this.findViewById(R.id.button1);
+        mButton2= (Button) this.findViewById(R.id.button2);
         mButton3 = (Button) this.findViewById(R.id.button3);
         mButton4 = (Button) this.findViewById(R.id.button4);
-        mScoreView = (TextView) this.findViewById(R.id.score);
-        mScoreText = (TextView) this.findViewById(R.id.score_text);
 
+        //if statement about SavedInstanceState but I'm not sure why/what
+
+        Context c = getApplicationContext();
+       // XMLQuizGenerator.createQuizes(c);
+        //parser shit
+
+        //then restore quiz
+    }
+
+    //NOT doing left/right click
+    public void click(View button){
+        //do something with Toast then
+        //updateScore();
 
     }
 
-    //update question method
-    //method to update score
+    public void updateScore(){
+        mQuestionNum += 1;
+        int numRemaining = mQuiz.size() - mQuestionNum;
+        String s = mCorrect+"/"+mQuiz.size()+" questions correct with "+numRemaining+" questions remaining";
+        mScoreView.setText(scoreBase+s);//may just need to remove scoreBase
+
+        if(mQuestionNum < mQuiz.size()){
+            askQuestion(); // will be made further down in this class
+        }
+
+        else{
+            Toast.makeText(MainActivity.this, "the end", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    //CREATE onCreateOptions
+    //Create onPrepareOptions
+    //create onOptionsItemSelected
+
+
+    private void newGame(String title ){
+        //line for XMLQuizGenerator
+        //fill in later
+
+    }
+
+    public void newGame(Quiz q){
+
+    }
+
+    private void askQuestion(){
+        Question q = mQuiz.getQuestion(mQuestionNum);
+        mQuestionView.setText(q.getQuestionPhrase());
+        String[] mAnswers = (String[]) q.getAnswers().keySet().toArray();
+        mButton1.setText(mAnswers[0]);
+        mButton2.setText(mAnswers[1]);
+        mButton3.setText(mAnswers[2]);
+        mButton3.setText(mAnswers[3]);
+    }
+
+    public void localCallback(String rawString){
+        Log.d("quizmaster", "local callback");
+        //the rest of the JSONParser
+    }
 
 }
+
