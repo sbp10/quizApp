583c04caed8e992fcb515563f48f7d2dfeeb5562
nschroder99 <36391944+nschroder99@users.noreply.github.com>
2018-02-24 16:12:00 -0500

    edited quiz to allow Linear and Personality children

--- app/src/main/java/com/example/sabrinapin/multquiz/Quiz.java
+++ app/src/main/java/com/example/sabrinapin/multquiz/Quiz.java
@@ -1,5 +1,6 @@
 package com.example.sabrinapin.multquiz;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -9,10 +10,16 @@ import java.util.Arrays;
 public class Quiz {
     private ArrayList<Question> mQuestions; //list of questions
     private final String mTitle; //title of quiz
+    int mScore;
+    int mQuestionNum;
+    int mNumRemaining;
 
     public Quiz(String name, Question[] qs)  {
         mTitle = name;
         mQuestions = new ArrayList<Question>(Arrays.asList(qs));
+        mScore = 0;
+        mQuestionNum = 0;
+        mNumRemaining = qs.length;
     }
 
     //how many questions are in the quiz
@@ -27,10 +34,33 @@ public class Quiz {
 
     //get me a mf question
     public Question getQuestion(int index){
-        if (0 <= index && index < myQuestions.size()){
-            return myQuestions.get(index);
+        if (0 <= index && index < mQuestions.size()){
+            return mQuestions.get(index);
         }
         throw new IndexOutOfBoundsException("bad index "+index);
     }
+
+    public void incrementScore(int i)  {
+        mScore += i;
+    }
+
+    public String updateScoreboard()  {
+        return null;
+    }
+
+    public String processResults()  {
+        return null;
+    }
+
+
+    public int getScore() {
+        return mScore;
+    }
+
+    public int getQuestionNum() {
+        return mQuestionNum;
+    }
+
+
 
 }
